befab2f3032387f673b63ec8fa831bb8a3a9ac46
Rosie Allott <rosie.allott@gmail.com>
2016-08-16 15:22:34 +0100

    Including attr_reader

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,9 +1,15 @@
 require './lib/bike'
 
 class DockingStation
+  attr_reader :has_bike
+
   def release_bike
     Bike.new
+    @has_bike = false
   end
+
   def dock_bike(bike)
+    @has_bike = true
   end
+
 end
