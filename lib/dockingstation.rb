7f26e1e76e1eee298ca4e4bcff9ca3a3e8c9ae11
BenRoss92 <benjaminjackross@gmail.com>
2016-08-18 16:54:56 +0100

    dealing with broken bikes - completed

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -5,14 +5,20 @@ class DockingStation
   attr_reader :capacity
   DEFAULT_CAPACITY = 20
 
-    def initialize(capacity = DEFAULT_CAPACITY)
-      @bikes = []
-      @capacity = capacity
-    end
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @bikes = []
+    @capacity = capacity
+  end
 
   def release_bike
     raise "no bikes" if @bikes.empty?
-    @bikes.pop
+    @bikes.each do |bike|
+      if bike.working? == false
+        raise "broken bike"
+      else
+        @bikes.pop
+      end
+    end
   end
 
   def dock_bike(bike)
